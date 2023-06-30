0495a397949564b8e22c794efbcf8bfc0d0e2d51
root <root@gitlab.example.com>
2023-06-30 08:45:29 +0200

    test

--- scan.sh
+++ scan.sh
@@ -1,6 +1,13 @@
 cat hosts | while read host;
 do
-    echo $host
-    # mkdir -p reports/$TARGET
-    # oscap-ssh --sudo vagrant@$TARGET 22 xccdf eval --datastream-id scap_org.open-scap_datastream_from_xccdf_ssg-debian11-xccdf.xml --xccdf-id scap_org.open-scap_cref_ssg-debian11-xccdf.xml --profile xccdf_org.ssgproject.content_profile_anssi_np_nt28_average --oval-results --results reports/$TARGET/xccdf-results.xml --results-arf reports/$TARGET/arf.xml --report reports/$TARGET/report.html /usr/local/share/xml/scap/ssg/content/ssg-debian11-ds.xml || (e=$? && if [[ $e -ne 2 ]]; then exit $e; fi )
+    mkdir -p reports/$host
+    oscap-ssh --sudo vagrant@$host 22 xccdf eval \ 
+        --datastream-id scap_org.open-scap_datastream_from_xccdf_ssg-debian11-xccdf.xml \
+        --xccdf-id scap_org.open-scap_cref_ssg-debian11-xccdf.xml \
+        --profile xccdf_org.ssgproject.content_profile_anssi_np_nt28_average \ 
+        --oval-results --results reports/$host/xccdf-results.xml \
+        --results-arf reports/$host/arf.xml \
+        --report reports/$host/report.html \ 
+        /usr/local/share/xml/scap/ssg/content/ssg-debian11-ds.xml \ 
+        || (e=$? && if [[ $e -ne 2 ]]; then exit $e; fi )
 done
