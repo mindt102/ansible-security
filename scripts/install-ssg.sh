f0634655a7cd8f773d3994e8269a4bbf8a4255e7
root <root@gitlab.example.com>
2023-07-08 16:20:13 +0200

    test download

--- scripts/install-ssg.sh
+++ scripts/install-ssg.sh
@@ -4,13 +4,13 @@ apt-get install -y curl # libxml2-utils # ninja-build xsltproc libopenscap8
 
 # # Download SCAP Security Guide source code
 ssg_version=$(curl --silent "https://api.github.com/repos/ComplianceAsCode/content/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-echo $ssg_version
-# ssg_url="https://github.com/ComplianceAsCode/content/releases/download/$ssg_version/scap-security-guide-$ssg_version.tar.bz2"
-# wget -qO /tmp/scap-security-guide-$ssg_version.tar.bz2 "$ssg_url"
-# tar -xjf /tmp/scap-security-guide-$ssg_version.tar.bz2 -C /tmp
+ssg_url="https://github.com/ComplianceAsCode/content/releases/download/$ssg_version/scap-security-guide-$ssg_version.tar.bz2"
+wget -qO /tmp/scap-security-guide-$ssg_version.tar.bz2 "$ssg_url"
+tar -xjf /tmp/scap-security-guide-$ssg_version.tar.bz2 -C /tmp
 
 # # Build SCAP Security Guide
-# mkdir -p /tmp/scap-security-guide-$ssg_version/build
+mkdir -p /tmp/scap-security-guide-$ssg_version/build
+ls -la /tmp/scap-security-guide-$ssg_version
 # cmake -S /tmp/scap-security-guide-$ssg_version -B /tmp/scap-security-guide-$ssg_version/build
 # make -C /tmp/scap-security-guide-$ssg_version/build -j4 debian11
 # # make -C /tmp/scap-security-guide-$ssg_version/build install
