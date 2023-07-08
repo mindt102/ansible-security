77d765fb022e378b8112d86a4dc8a95313ec3390
root <root@gitlab.example.com>
2023-07-08 16:23:08 +0200

    change to curl

--- scripts/install-ssg.sh
+++ scripts/install-ssg.sh
@@ -5,7 +5,7 @@ apt-get install -y curl # libxml2-utils # ninja-build xsltproc libopenscap8
 # # Download SCAP Security Guide source code
 ssg_version=$(curl --silent "https://api.github.com/repos/ComplianceAsCode/content/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 ssg_url="https://github.com/ComplianceAsCode/content/releases/download/$ssg_version/scap-security-guide-$ssg_version.tar.bz2"
-wget -qO /tmp/scap-security-guide-$ssg_version.tar.bz2 "$ssg_url"
+curl -so /tmp/scap-security-guide-$ssg_version.tar.bz2 "$ssg_url"  
 tar -xjf /tmp/scap-security-guide-$ssg_version.tar.bz2 -C /tmp
 
 # # Build SCAP Security Guide
